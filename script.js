e8c498f46dfcb26bf628099788f9f3d5d366f8d4
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-04-04 13:49:46 -0500

    Mobile

--- script.js
+++ script.js
@@ -4,11 +4,12 @@
 // 	duration: 1.2,
 // 	'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
 // });
+window.onload = function () {
+	window.scrollTo(0, 0);
+};
 
 const nav = document.querySelector('.nav');
 const navList = document.querySelectorAll('.nav-list');
-const navItem = document.querySelectorAll('.nav ul li a');
-const burger = document.querySelector('.burger');
 const logo = document.querySelector('.logo');
 
 window.addEventListener('scroll', sizeNav);
@@ -23,3 +24,41 @@ function sizeNav() {
 		logo.style.width = '220px';
 	}
 }
+
+//~ mobile nav
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const navList = document.querySelector('.nav-list');
+	const navItem = document.querySelectorAll('.nav-list a');
+
+	burger.addEventListener('click', () => {
+		navList.classList.toggle('open');
+		burger.classList.toggle('toggle');
+	});
+	//close nav when nav-link clicked
+	for (let i = 0; i < navItem.length; i++) {
+		let closeNav = navItem[i];
+
+		closeNav.addEventListener('click', () => {
+			navList.classList.toggle('open');
+			burger.classList.toggle('toggle');
+		});
+	}
+};
+navSlide();
+
+//~ mobile vh toolbar fix
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+// GSAP
+gsap.to('.nav', {
+	y: 0,
+	opacity: 1,
+	duration: 1,
+});
